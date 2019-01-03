a501377d900e26136fdc263f2407119cbd234d2d
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-01-04 00:01:14 +0600

    Solve Insertion Error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'utils/databasehelper.dart';
+import 'models/user.dart';
 
-main(){
+
+main() async{
+
+  var db = new DatabaseHelper();
+
+  // Add user
+  int saveuser = await db.saveUser(new User("Kamrul","1234"));
+  print("$saveuser :User saved");
+
+//  // List User
+  List users = await db.getAllUsers();
+  print(users);
+
   runApp(new MaterialApp(
     title: "Relational Databas",
     home: Home(),
--- lib/utils/databasehelper.dart
+++ lib/utils/databasehelper.dart
@@ -1,93 +1,93 @@
 import 'dart:async';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'dart:io';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 import '../models/user.dart';
 
-// Skeleton Style
-class DatabaseHelper{
 
-  final String tableUser = "userTable";
-  final String columnId = "id";
-  final String columnUser = "username";
-  final String columnPassword = "password";
+class DatabaseHelper {
 
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
   factory DatabaseHelper() => _instance;
+  final String tableUser = "userTable";
+  final String columnId = "id";
+  final String columnUsername = "username";
+  final String columnPassword = "password";
 
   static Database _db;
-  Future<Database> get db async{
-    if(_db != null){
+
+  Future<Database> get db async {
+    if (_db != null) {
       return _db;
     }
     _db = await initDb();
     return _db;
   }
+
   DatabaseHelper.internal();
 
   initDb() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentDirectory.path,"maindb.db");
-    var Db = await openDatabase(path,version: 1,onCreate: _oncreate);
-
-
+    String path = join(documentDirectory.path, "maindb.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
   }
 
-  // Create Database
-  void _oncreate(Database db, int version) async {
-    String sql = "CREATE TABLE $tableUser ($columnId INTEGER PRIMARY KEY, $columnUser TEXT,$columnPassword TEXT)";
-    await db.execute(sql);
+
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUsername TEXT, $columnPassword TEXT)");
   }
 
-  // Insert Data Into Database
-  Future<int> saveUser (User user) async{
+
+  //Insertion
+  Future<int> saveUser(User user) async {
     var dbClient = await db;
-    int res = await dbClient.insert(tableUser, user.toMap());
+    int res = await dbClient.insert("$tableUser", user.toMap());
     return res;
   }
 
-  // Read Data -> Get User
-  Future<List> getAllusers() async{
+  //Get Users
+  Future<List> getAllUsers() async {
     var dbClient = await db;
-    String sql = "SELECT * FROM $tableUser";
-    var result = await dbClient.rawQuery(sql);
+    var result = await dbClient.rawQuery("SELECT * FROM $tableUser");
+
     return result.toList();
   }
 
-  // Read Data Only One User
-  Future<User> getUser(int id) async{
+  Future<int> getCount() async {
     var dbClient = await db;
-    String sql = "SELECT * FROM $tableUser WHERE $columnId = $id";
-    var result = await dbClient.rawQuery(sql);
-    if(result.length == 0){
-      return null;
-    }
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("SELECT COUNT(*) FROM $tableUser"));
+  }
+
+  Future<User> getUser(int id) async {
+    var dbClient = await db;
+
+    var result = await dbClient.rawQuery("SELECT * FROM $tableUser WHERE $columnId = $id");
+    if (result.length == 0) return null;
     return new User.fromMap(result.first);
   }
 
-  // Counting All The User
-  Future<int> getCount() async{
+  Future<int> deleteUser(int id) async {
     var dbClient = await db;
-    return Sqflite.firstIntValue(
-      await dbClient.rawQuery("SELECT COUNT(*) FROM $tableUser"));
+
+    return await dbClient.delete(tableUser,
+        where: "$columnId = ?", whereArgs: [id]);
   }
 
-  // Update Data
-  Future<int> updateUser(User user) async{
+
+  Future<int> updateUser(User user) async {
     var dbClient = await db;
-    return await dbClient.update(tableUser, user.toMap(),where: "$columnId = ?",whereArgs: [user.id]);
+    return await dbClient.update(tableUser,
+        user.toMap(), where: "$columnId = ?", whereArgs: [user.id]);
   }
 
-  // Delete Data
-  Future<int> deleteuser(int id) async{
-    var dbClient = await db;
-    return await dbClient.delete(tableUser,where: "$columnId = ?",whereArgs: [id]);
-  }
-
-  Future close() async{
+  Future close() async {
     var dbClient = await db;
     return dbClient.close();
   }
+
 
 }
