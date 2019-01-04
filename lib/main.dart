9d55df16b64e495417f7abcfa3e7d0992adbc8ac
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-01-04 20:26:16 +0600

    Showing data in list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'utils/databasehelper.dart';
 import 'models/user.dart';
 
+List users;
 
 main() async{
 
   var db = new DatabaseHelper();
 
   // Add user
-  int saveuser = await db.saveUser(new User("Kamrul","1234"));
-  print("$saveuser :User saved");
+//  int saveuser = await db.saveUser(new User("Kamrul","1234"));
+//  print("$saveuser :User saved");
 
 //  // List User
-  List users = await db.getAllUsers();
+  users = await db.getAllUsers();
   print(users);
 
   runApp(new MaterialApp(
@@ -31,7 +30,20 @@ class Home extends StatelessWidget {
         title: Text("Relational Database"),
         centerTitle: true,
       ),
-
+      body: new ListView.builder(
+        itemCount: users.length,
+        itemBuilder:(_,int position){
+          return Card(
+            child: ListTile(
+              title: Text(User.fromMap(users[position]).username),
+              leading: CircleAvatar(child: Text(User.fromMap(users[position]).username[0]),),
+              trailing: CircleAvatar(child: Text(User.fromMap(users[position]).password,style: TextStyle(color: Colors.white),)
+                ,backgroundColor: Colors.cyan,),
+              onTap: (){},
+            ),
+          );
+        },
+      ),
     );
   }
 }
